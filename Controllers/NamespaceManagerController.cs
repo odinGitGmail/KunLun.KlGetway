6c7fca12baf41fc69959c9af10b789167facfe61
odinsam <123679@qq.com>
2024-01-21 03:11:07 +0800

    no message

---
+++ Controllers/NamespaceManagerController.cs
@@ -0,0 +1,79 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Cola.ColaNacos;
+using Cola.ColaWebApi;
+using Cola.Core.Models;
+using Cola.Core.Models.ColaNacos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KunLun.KlGetway.Controllers;
+
+[ApiController]
+[Route("nacos")]
+public class NamespaceManagerController: ControllerBase
+{
+    private readonly IWebApi _webApi;
+    private readonly IColaNacos _colaNacos;
+    public NamespaceManagerController(IWebApi webApi, IColaNacos colaNacos)
+    {
+        _webApi = webApi.GetClient("ColaNacos");
+        _colaNacos = colaNacos;
+    }
+
+    /// <summary>
+    /// 查询命名空间列表
+    /// </summary>
+    /// <returns>ApiResult&lt;List&lt;NamespaceList&gt;&gt;</returns>
+    [HttpGet]
+    [Route("v{majorVersion}/console/namespace/list")]
+    public ApiResult<List<NacosNamespaceInfo>>? GetAllNamespace()
+    {
+        return _colaNacos.NamespaceQuery("ColaNacos");
+    }
+    
+    /// <summary>
+    /// 查询命名空间列表
+    /// </summary>
+    /// <returns>ApiResult&lt;NamespaceList&gt;</returns>
+    [HttpGet]
+    [Route("v{majorVersion}/console/namespace")]
+    public ApiResult<NacosNamespaceInfo>? GetNamespaceById(string? namespaceId)
+    {
+        return _colaNacos.NamespaceQueryById("ColaNacos",namespaceId);
+    }
+    
+    /// <summary>
+    /// 创建命名空间
+    /// </summary>
+    /// <param name="nacosNamespace">请求Body</param>
+    /// <returns>是否执行成功</returns>
+    [HttpPost]
+    [Route("v{majorVersion}/console/namespace")]
+    public ApiResult<bool>? AddNamespace([FromBody] NacosNamespace nacosNamespace)
+    {
+        return _colaNacos.CreateNamespace("ColaNacos",nacosNamespace);
+    }
+    
+    /// <summary>
+    /// 编辑命名空间
+    /// </summary>
+    /// <param name="nacosNamespace">请求Body</param>
+    /// <returns>是否执行成功</returns>
+    [HttpPut]
+    [Route("v{majorVersion}/console/namespace")]
+    public ApiResult<bool>? UpdateNamespace([FromBody] NacosNamespace nacosNamespace)
+    {
+        return _colaNacos.UpdateNamespace("ColaNacos",nacosNamespace);
+    }
+    
+    /// <summary>
+    /// 删除命名空间
+    /// </summary>
+    /// <param name="namespaceId">namespaceId</param>
+    /// <returns>是否执行成功</returns>
+    [HttpDelete]
+    [Route("v{majorVersion}/console/namespace")]
+    public ApiResult<bool>? DeleteNamespace([FromQuery][Required] string namespaceId)
+    {
+        return _colaNacos.DeleteNamespace("ColaNacos", namespaceId);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,24 +1,33 @@
 using Cola.ColaMiddleware.ColaSwagger;
+using Cola.ColaMiddleware.ColaVersioning;
+using Cola.ColaNacos;
+using Cola.ColaWebApi;
+using Cola.Core;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("appsettings.json");
 
+var config = builder.Configuration;
 builder.Host
     .ConfigureLogging((hostingContext, build) =>
     {
         //该方法需要引入Microsoft.Extensions.Logging名称空间
         build.AddFilter("System", LogLevel.Error); //过滤掉系统默认的一些日志
         build.AddFilter("Microsoft", LogLevel.Error); //过滤掉系统默认的一些日志
-    })
-    .ConfigureAppConfiguration((context, build) =>
-    {
-        var environmentName = builder.Environment.EnvironmentName;
-        build.AddNacosV2Configuration(build.Build().GetSection($"NacosConfig-{environmentName}"));
     });
-
+builder.Services.AddColaCore();
+builder.Services.AddColaHttpClient(config);
+var environmentName = builder.Environment.EnvironmentName;
+// builder.Configuration.AddNacosV2Configuration(config.GetSection($"NacosConfig-{environmentName}"))
+// builder.Services.AddNacosV2Config(config.GetSection($"ColaNacos:a"));
+// builder.Services.AddNacosV2Config(config.GetSection($"ColaNacos:nacosServer"));
+builder.Services.AddColaNacos(config, new List<string>() { "a", "nacosServer" });
 // Add services to the container.
-builder.Configuration.AddJsonFile("appsettings.json");
-var config = builder.Configuration;
+
+
 builder.Services.AddControllers();
+
+builder.Services.AddColaVersioning(config);
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
